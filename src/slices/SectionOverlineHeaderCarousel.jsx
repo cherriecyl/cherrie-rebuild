0091b32a435a725344712a91620b39d95b14ac60
Cherrie Lam <cherrieclam@gmail.com>
2021-10-03 23:32:41 -0700

    CarouselSection - Don't show HTML div if no text

--- src/slices/SectionOverlineHeaderCarousel.jsx
+++ src/slices/SectionOverlineHeaderCarousel.jsx
@@ -5,7 +5,7 @@ import { ProjectSection, GridWrap, Lightbox, Carousel } from '../components'
 const SectionOverlineHeaderCarousel = ({ input }) => (
 
   <ProjectSection className={input.primary.background} id={input.primary.section_id}>
-      <GridWrap>
+      <GridWrap className="columngapS">
           <div className="grid8L start3L grid10I start2I grid12T start1T">
               {`${input.primary.section_overline.text}` == "" ? `` : <h2 className="overline">{input.primary.section_overline.text}</h2>} 
               <h3>{input.primary.section_large_subtitle.text}</h3>
@@ -14,12 +14,12 @@ const SectionOverlineHeaderCarousel = ({ input }) => (
           {input.items.map((item) => (
                   item.image_collection == null ? 
                   <>
-                    <div className="grid8L start3L grid10I start2I grid12T start1T" dangerouslySetInnerHTML={ { __html: item.body_text.html} } />
+                    {item.body_text.html == null ? `` : <div className="grid8L start3L grid10I start2I grid12T start1T" dangerouslySetInnerHTML={ { __html: item.body_text.html} } />}
                     <Lightbox boxClass={item.grid_definition.text} imgClass={item.img_class.text} src={item.image.localFile.childImageSharp.fluid} alt={item.image.alt}/>
                   </>
                   : 
                   <>
-                    <div className="grid8L start3L grid10I start2I grid12T start1T" dangerouslySetInnerHTML={ { __html: item.body_text.html} } />
+                    {item.body_text.html == null ? `` : <div className="grid8L start3L grid10I start2I grid12T start1T" dangerouslySetInnerHTML={ { __html: item.body_text.html} } />}
                     <Carousel className={item.grid_definition.text} input={item.image_collection.document[0].data.image_collection}/>
                   </>
           ))}
