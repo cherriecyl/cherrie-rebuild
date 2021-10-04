532bdb689a376e8992a514eb6ea53b25e917769e
Cherrie Lam <cherrieclam@gmail.com>
2021-10-03 19:56:09 -0700

    Update and style carousel component

--- src/components/_ui/Carousel.jsx
+++ src/components/_ui/Carousel.jsx
@@ -1,43 +1,43 @@
 import React, { useState } from 'react';
-// import styled from "@emotion/styled";
+import styled from "@emotion/styled";
 // import PropTypes from "prop-types";
-// import ImgWithCaption from './ImgWithCaption';
+import ImgWithCaption from './ImgWithCaption';
+import Lightbox from './Lightbox';
 import Carousel, { Dots } from '@brainhubeu/react-carousel';
 import '@brainhubeu/react-carousel/lib/style.css';
 
-class MyCarousel extends React.Component {
-    constructor() {
-      super()
-      this.state = {
-        value: 0,
-        slides: [
-          (<img src="https://images.unsplash.com/photo-1593642532454-e138e28a63f4?ixid=MnwxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwxfHx8ZW58MHx8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=900&q=60" />),
-          (<img src="https://images.unsplash.com/photo-1633190223932-107ab48257b2?ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyfHx8ZW58MHx8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=900&q=60" />),
-          (<img src="https://images.unsplash.com/photo-1633285718604-11e91bc5b126?ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwzfHx8ZW58MHx8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=900&q=60" />),
-        ],
-      }
-      this.onchange = this.onchange.bind(this);
+const CarouselContainer = styled("div")`
+    max-width: 100%;
+    .BrainhubCarouselItem > div {
+        width: 100%;
+        padding: 1em;
     }
-  
-  
-    onchange(value) {
-      this.setState({ value });
+    .BrainhubCarousel__arrows {
+        background: none;
+        span {
+            border-color: ${(props) => props.theme.colors.grey700};
+        }
+        &:hover {
+            background-color: ${(props) => props.theme.colors.grey700};
+        }
+        &:disabled {
+            background: none;
+            span {
+                border-color: ${(props) => props.theme.colors.gray500};
+            }
+        }
     }
-  
-    render() {
-      return (
-      <div>
-        <Carousel
-          plugins={['arrows']}
-          value={this.state.value}
-          slides={this.state.slides}
-          onChange={this.onchange}
-        />
-        <Dots value={this.state.value} onChange={this.onchange} number={this.state.slides.length} />
-      </div>
-      );
-    }
-  }
+`
+
+const MyCarousel = ({ className, input }) => (
+    <CarouselContainer className={className}>
+        <Carousel plugins={['arrows']}>
+            {input.map((item) => (
+                <Lightbox src={item.image.localFile.childImageSharp.fluid} alt={item.image.alt} imgClass="centerCaption"/>
+            ))}
+        </Carousel>
+    </CarouselContainer>
+  );
 
 // Carousel.propTypes = {
 //     // alt: PropTypes.string.isRequired,
