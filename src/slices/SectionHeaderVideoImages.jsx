e2a1ee9cec27606f40e213d4400bfd1109cba9a6
Cherrie Lam <cherrieclam@gmail.com>
2021-10-06 17:21:32 -0700

    Pull Overline to the left

--- src/slices/SectionHeaderVideoImages.jsx
+++ src/slices/SectionHeaderVideoImages.jsx
@@ -7,8 +7,10 @@ const SectionHeaderVideoImages= ({ input }) => (
 
   <ProjectSection className={input.primary.background} id={input.primary.section_id}>
       <GridWrap>
-          <div className="grid8L grid10I grid12T start3L start2I start1T">
+          <div className="grid1L grid10I start2I grid12T start1T sectionOverline">
               <h2 className="overline">{input.primary.section_overline.text}</h2>
+          </div>
+          <div className="grid8L grid10I grid12T start3L start2I start1T">
               <h3>{input.primary.section_large_subtitle.text}</h3>
               <div dangerouslySetInnerHTML={ { __html: input.primary.body_text.html} } />
           </div>
--- src/slices/SectionPrototype.jsx
+++ src/slices/SectionPrototype.jsx
@@ -6,8 +6,10 @@ const SectionPrototype = ({ input }) => {
   return (
     <ProjectSection className={input.primary.background} id={input.primary.section_id}>
       <GridWrap className="dense">
+            <div className="grid1L grid10I start2I grid12T start1T sectionOverline">
+              <h2 className="overline">{input.primary.section_overline.text}</h2>
+            </div>
             <div className="grid8L grid10I start3L start2I grid12T start1T">
-                <h2 className="overline">{input.primary.section_overline.text}</h2>
                 <h3>{input.primary.section_large_subtitle.text}</h3>
                 <div dangerouslySetInnerHTML={ { __html: input.primary.body_text.html} } />
             </div>
