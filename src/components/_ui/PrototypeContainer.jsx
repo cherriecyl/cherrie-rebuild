70c575f9198e3cdd86e386640bc2055f3e283d8d
Cherrie Lam <cherrieclam@gmail.com>
2021-10-06 17:20:27 -0700

    Add autoPlay loop on prototype videos

--- src/components/_ui/PrototypeContainer.jsx
+++ src/components/_ui/PrototypeContainer.jsx
@@ -70,7 +70,7 @@ const PrototypeContainer = ({ className, device, prototype, video, background })
                         Your browser does not support the video tag.
                     </video>
                     :
-                    <video poster={background.url} width="360" height="640" muted controls>
+                    <video poster={background.url} width="360" height="640" autoPlay loop muted controls>
                         <source src={video.url} type="video/mp4"/>
                         Your browser does not support the video tag.
                     </video> }
