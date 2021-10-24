66df303e54bfeba51886ca16aa071657100a6ca0
Cherrie Lam <cherrie@peacegeeks.org>
2021-10-24 00:49:14 -0700

    Add Side Bkg Image to HMW

--- src/slices/SectionHMW.jsx
+++ src/slices/SectionHMW.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import styled from '@emotion/styled'
-import { ProjectSection, GridWrap, TextBox } from '../components'
+import { ProjectSection, GridWrap, ImgContainer } from '../components'
 
 
 const Wrap = styled("div")`
@@ -12,9 +12,6 @@ const Wrap = styled("div")`
     color: ${(props) => props.theme.colors.grey700};
     font-weight: 700;
     font-family: 'Manrope', 'Inter', sans-serif;
-  }
-  p {
-    margin-bottom: 0;
   }
   li {
     text-align: left;
@@ -30,24 +27,54 @@ const Wrap = styled("div")`
   li:last-of-type {
     margin-bottom:0;
   }
+  p:last-of-type {
+    margin-bottom: 0;
+  }
   ul, ol {
     margin-bottom: 0;
   }
 `
 
+const BkgImgWrap = styled("div")`
+  position: absolute;
+  width: 15vw;
+  max-width: 250px;
+  right: 0;
+  div { 
+    right: -2vw;
+  }
+  z-index: 1;
+  margin: auto;
+  padding-top: 7em;
+  overflow: hidden;
+  @media(max-width: ${(props) => props.theme.maxwidthiPadPro}) {
+    display: none;
+    width: 0;
+    height: 0;
+    visibility: hidden;
+  }
+`
+
 const SectionHMW = ({ input }) => (
+  <>
+  {input.primary.background_image_side.url == null ? 
+    `` :
+    <BkgImgWrap aria-hidden="true">
+        <ImgContainer src={input.primary.background_image_side.localFile.childImageSharp.fluid} alt={input.primary.background_image_side.alt} />
+    </BkgImgWrap>}
 
   <ProjectSection id={input.primary.section_id} className={input.primary.background}>
       <GridWrap>
+        {input.primary.section_id == `focus` ? <div className="grid12L"><p className="overline fontBigger">How did we get here?</p></div> : ``}
           <div className="grid1L grid10I start2I grid12T start1T sectionOverline">
               <h2 className="overline">{input.primary.section_overline.text}</h2>
           </div>
-          <div className="grid8L grid10I grid1T start3L start2I start1T">
-              <Wrap className="brown" dangerouslySetInnerHTML={ { __html: input.primary.section_large_subtitle.html} } />
+          <div className="grid8L grid10I grid12T start3L start2I start1T">
+              <Wrap dangerouslySetInnerHTML={ { __html: input.primary.section_large_subtitle.html} } />
           </div>
       </GridWrap>
   </ProjectSection>
-
+  </>
 )
 
 export default SectionHMW;
