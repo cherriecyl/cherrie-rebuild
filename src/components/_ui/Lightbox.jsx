02290e6613dd1bf6bdb15891bb785406bdb2b4d9
Cherrie Lam <cherrie@peacegeeks.org>
2021-10-24 00:45:04 -0700

    Update lightbox so button is around the image, not the caption

--- src/components/_ui/Lightbox.jsx
+++ src/components/_ui/Lightbox.jsx
@@ -5,6 +5,65 @@ import VisuallyHidden from "@reach/visually-hidden";
 import "@reach/dialog/styles.css"
 import PropTypes from "prop-types";
 import ImgWithCaption from './ImgWithCaption';
+import ImgContainer from './ImgContainer'
+
+const ImgWithCaptionWrap = styled("figure")`
+    display: flex;
+    flex-direction: column;
+    height: 100%;
+    &.stretch {
+        div {
+            height: 100%;
+        }
+    }
+    margin: 0;
+    figcaption {
+        margin-top: 0.5em;
+        margin-bottom: 0.5em;
+        color: ${(props) => props.theme.colors.grey700};
+        font-size: 0.9em;
+        font-family: 'Atkinson Hyperlegible', 'Inter', sans-serif;
+    }
+    &.centerCaption {
+        figcaption {
+            text-align: center;
+        }
+    }
+    &.topCaption {
+        flex-direction: column-reverse;
+    }
+    &.hero {
+        margin-top: 5em;
+        box-shadow: 0px 9px 24px rgba(0, 0, 0, 0.1), -1em -1em 0 ${(props) => props.theme.colors.grey300};
+        border-radius: 4px;
+        @media(max-width:${(props) => props.theme.maxwidthTablet}) {
+            box-shadow: 0px 0px 0px rgba(0, 0, 0, 0.1), 0em 0em 0 ${(props) => props.theme.colors.grey300};
+            div {
+                border-radius: 0px;
+            }
+            width: 100vw;
+            left: 50%;
+            right: 50%;
+            margin-left: -50vw;
+            margin-right: -50vw;
+            border-radius: 0px;
+            position: relative;
+        }
+    }
+    &.border {
+        button > div {
+            border: 2px solid ${(props) => props.theme.colors.grey200}; 
+        }
+    }
+    &.altShadow:hover {
+        div {
+            box-shadow: 0px 9px 24px rgba(0, 0, 0, 0.1);
+            transition: all 250ms ease-in-out;
+            transform: translateY(-2px);
+            -webkit-transform: translateY(-2px);
+        }
+    }
+`
 
 const PreviewButton = styled("button")`
     background: transparent;
@@ -15,8 +74,8 @@ const PreviewButton = styled("button")`
     &:hover {
         color: ${(props) => props.theme.colors.grey900};
         cursor: zoom-in;
-        figure > div > div {
-            transform: scale(1.1);
+        div > div {
+            transform: scale(1.02);
             transition: transform 200ms ease-in-out;
         }
     }
@@ -34,6 +93,11 @@ const LightboxDialogContent = styled(DialogContent)`
     background: none;
     figure > div {
         background: white;
+    }
+    &.clear {
+        figure > div {
+            background:none;
+        }
     }
     figure > figcaption {
         color: white;
@@ -62,7 +126,7 @@ const LightboxDialogOverlay = styled(DialogOverlay)`
     z-index: 5;
 `
 
-const Lightbox = ({ boxClass, imgClass, src, alt, showcap }) => {
+const Lightbox = ({ boxClass, imgClass, src, alt, showcap, lbBkg }) => {
     const [showDialog, setShowDialog] = useState(false);  
     const open = (e) => { 
         setShowDialog(true); 
@@ -81,19 +145,23 @@ const Lightbox = ({ boxClass, imgClass, src, alt, showcap }) => {
 
     return ( 
         <div className={boxClass}>      
-            <PreviewButton type="button" onClick={open}>
-                <ImgWithCaption src={src} className={imgClass} alt={alt} showcap={showcap} />
-            </PreviewButton>
+            <ImgWithCaptionWrap className={imgClass}>
+                <PreviewButton type="button" onClick={open}>
+                    <ImgContainer src={src} alt={alt} />
+                </PreviewButton>
+                {showcap === false ? `` : <figcaption>{alt}</figcaption>}
+            </ImgWithCaptionWrap>
+    
             
             <LightboxDialogOverlay isOpen={showDialog} onDismiss={close}>     
-                <LightboxDialogContent allowPinchZoom={true} onLoad={unfocus}> 
+                <LightboxDialogContent allowPinchZoom={true} onLoad={unfocus} className={lbBkg}> 
                     <div>
                         <button id="button" onClick={close}>
                             <VisuallyHidden>Close</VisuallyHidden>
                             <span aria-hidden>×</span>
                         </button>
                     </div>
-                    <ImgWithCaption src={src} alt={alt} />       
+                    <ImgWithCaption src={src} alt={alt}/>       
                 </LightboxDialogContent>     
             </LightboxDialogOverlay>    
         </div>    
