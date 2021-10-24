4868a5133d50980294c72f0a1584c9007f3a8a79
Cherrie Lam <cherrie@peacegeeks.org>
2021-10-24 00:51:31 -0700

    Remove image from Quote

--- src/slices/SectionQuote.jsx
+++ src/slices/SectionQuote.jsx
@@ -8,12 +8,6 @@ const SectionQuote = ({ input }) => (
   <ProjectSection className={input.primary.background} id={input.primary.section_id}>
       <GridWrap>
           <Quote className="grid6L start4L grid10I start2I" text={input.primary.quote_text.html} img={input.primary.avatar} attr={input.primary.quote_attribute.text} />
-          {input.items.map((item) => (
-              item.image.alt == null ? 
-              ``
-              :
-              <Lightbox boxClass={`${item.grid_definition.text}` + ` sectionAbove`} imgClass={item.img_class.text} src={item.image.localFile.childImageSharp.fluid} alt={item.image.alt}/>
-          ))}
       </GridWrap>
   </ProjectSection>
 
